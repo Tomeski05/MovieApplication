d05a602746cddb44962098a28fd6353166d4242b
Filip Tomeski <tomeski@protonmail.com>
2022-05-01 23:32:55 +0200

    MovieApp

--- MovieApp/Controllers/MovieController.cs
+++ MovieApp/Controllers/MovieController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MovieApp.Domain;
+using MovieApp.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +10,98 @@ namespace MovieApp.Controllers
 {
     public class MovieController : Controller
     {
-        public IActionResult Index()
+        private readonly IMovieService _movieService;
+
+        public MovieController(IMovieService movieService)
+        {
+            _movieService = movieService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _movieService.GetAllProducts());
+        }
+
+        public async Task<IActionResult> Details(int id)
+        {
+            return View(await _movieService.GetProductById(id));
+        }
+
+        public IActionResult Create()
         {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Movies product)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    await _movieService.CreateProductAsync(product);
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "Unable to save changes. " +
+                                             "Try again, and if the problem persists " +
+                                             "see your system administrator.");
+            }
+            return View(product);
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            return View(await _movieService.GetProductById(id));
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Movies movie)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var dbMovies = await _movieService.GetProductById(id);
+                    if (await TryUpdateModelAsync<Movies>(dbMovies))
+                    {
+                        await _movieService.UpdateProductAsync(dbMovies);
+                        return RedirectToAction(nameof(Index));
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "Unable to save changes. " +
+                                             "Try again, and if the problem persists " +
+                                             "see your system administrator.");
+            }
+            return View(movie);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(int id)
+        {
+            try
+            {
+                var dbMovies = await _movieService.GetProductById(id);
+                if (dbMovies != null)
+                {
+                    await _movieService.DeleteProductAsync(dbMovies);
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "Unable to delete. " +
+                                             "Try again, and if the problem persists " +
+                                             "see your system administrator.");
+            }
+
+            return RedirectToAction(nameof(Index));
         }
     }
 }
--- MovieApp/Startup.cs
+++ MovieApp/Startup.cs
@@ -4,6 +4,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MovieApp.Repositories;
+using MovieApp.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,6 +26,9 @@ namespace MovieApp
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddScoped<IMovieRepository, MovieRepository>();
+            services.AddScoped<IMovieService, MovieService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
